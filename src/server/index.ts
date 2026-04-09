ef4b7aff7458f4930b283e94f23823228034ac61
tr83 <teroriip@gmail.com>
2026-04-09 21:33:17 +0300

    Server connection

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,22 @@
+import amqp from "amqplib";
+
 async function main() {
-  console.log("Starting Peril server...");
+  const rabbitConnString = "amqp://guest:guest@localhost:5672/";
+  const conn = await amqp.connect(rabbitConnString);
+  console.log("Peril game server connected to RabbitMQ!");
+
+  ["SIGINT", "SIGTERM"].forEach((signal) =>
+    process.on(signal, async () => {
+      try {
+        await conn.close();
+        console.log("RabbitMQ connection closed.");
+      } catch (err) {
+        console.error("Error closing RabbitMQ connection:", err);
+      } finally {
+        process.exit(0);
+      }
+    }),
+  );
 }
 
 main().catch((err) => {
