bf14d43a00054dec48f73fe5411afb99bcecdd6a
Tero Riipinen <teroriip@gmail.com>
2026-05-03 13:22:37 +0300

    Client work

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,5 +1,39 @@
+import amqp from "amqplib";
+import { clientWelcome } from "../internal/gamelogic/gamelogic.js";
+import { declareAndBind, SimpleQueueType } from "../internal/pubsub/publish.js";
+import { ExchangePerilDirect, PauseKey } from "../internal/routing/routing.js";
+
 async function main() {
   console.log("Starting Peril client...");
+    const rabbitConnString = "amqp://guest:guest@localhost:5672/";
+    const conn = await amqp.connect(rabbitConnString);
+
+    ["SIGINT", "SIGTERM"].forEach((signal) =>
+      process.on(signal, async () => {
+        try {
+          await conn.close();
+          console.log("RabbitMQ connection closed.");
+        } catch (err) {
+          console.error("Error closing RabbitMQ connection:", err);
+        } finally {
+          process.exit(0);
+        }
+      }),
+    );
+
+    try {
+      const userName = await clientWelcome();
+
+      await declareAndBind(
+        conn,
+        ExchangePerilDirect,
+        `pause.${userName}`,
+        PauseKey,
+        SimpleQueueType.Transient,
+      )
+    } catch (err) {
+      console.error("Error:", err);
+    }
 }
 
 main().catch((err) => {
