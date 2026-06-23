6526642d14ef57ded90c8218b2c936a7c1ecb50c
Tero Riipinen <teroriip@gmail.com>
2026-06-23 20:00:58 +0300

    publishMsgPack function

--- src/internal/pubsub/publish.ts
+++ src/internal/pubsub/publish.ts
@@ -1,4 +1,5 @@
 import amqp, { type Channel, type ConfirmChannel } from "amqplib";
+import { encode } from "@msgpack/msgpack";
 
 export function publishJSON<T>(
   ch: ConfirmChannel,
@@ -14,6 +15,31 @@ export function publishJSON<T>(
       routingKey,
       content,
       { contentType: "application/json" },
+      (err) => {
+        if (err !== null) {
+          reject(new Error("Message was NACKed by the broker"));
+        } else {
+          resolve();
+        }
+      },
+    );
+  });
+}
+
+export function publishMsgPack<T>(
+  ch: ConfirmChannel,
+  exchange: string,
+  routingKey: string,
+  value: T,
+): Promise<void> {
+  const content = Buffer.from(encode(value));
+
+  return new Promise((resolve, reject) => {
+    ch.publish(
+      exchange,
+      routingKey,
+      content,
+      { contentType: "application/x-msgpack" },
       (err) => {
         if (err !== null) {
           reject(new Error("Message was NACKed by the broker"));
